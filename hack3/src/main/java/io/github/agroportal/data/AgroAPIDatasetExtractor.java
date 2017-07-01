5898f43aab99743e394fc4b7c10d20d0257d8634
Andon Tchechmedjiev <twk.theainur@gmail.com>
2017-07-01 14:31:09 +0200

    AgroAPIExtractor

--- hack3/src/main/java/io/github/agroportal/data/AgroAPIDatasetExtractor.java
+++ hack3/src/main/java/io/github/agroportal/data/AgroAPIDatasetExtractor.java
@@ -1,8 +1,25 @@
 package io.github.agroportal.data;
 
 
-public class AgroAPIDatasetExtractor {
-    public AgroAPIDatasetExtractor() {
+import io.github.agroportal.api.data.Dataset;
+import io.github.agroportal.api.data.DatasetExtractor;
+
+public class AgroAPIDatasetExtractor implements DatasetExtractor{
+
+
+    private final String downloadURL;
+
+    @SuppressWarnings("HardcodedFileSeparator")
+    public AgroAPIDatasetExtractor(final String apiUrl, final String datasetName) {
+        downloadURL = String.format("%s/explore/dataset/%s/download/?format=csv&use_labels_for_header=true",apiUrl,datasetName);
     }
+
+    @Override
+    public Dataset extract() {
+        return null;
+    }
+
+
+    //explore/dataset/liste-des-giee-labellises/download/?format=csv&timezone=Europe/Berlin&use_labels_for_header=true
 
 }
